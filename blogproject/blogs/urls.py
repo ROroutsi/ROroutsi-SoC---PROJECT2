d2d23cf6620bd3b664e9979839fb1c1daabd70c4
Romario Oroutsi <romario.oruci@gmail.com>
2020-03-29 04:54:55 +0300

    first commit

--- blogproject/blogs/urls.py
+++ blogproject/blogs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('like/', views.likes, name='like'),
     path('article/<int:id>/<slug:slug>/', views.slug, name='slug')
 ]
